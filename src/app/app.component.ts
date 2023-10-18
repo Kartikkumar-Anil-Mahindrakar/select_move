d56b320790e647b43672b90eefae8fa5be7f31fd
Kartik Mahindrakar <kartikmahindrakar@gmail.com>
2023-10-19 01:48:55 +0530

    'origin/main'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'select';
+  list1 = ['Akshay',"Divya","Amin"];
+  list2 = ["Piku","Ajay","Dipika"];
+  leftSelectedList: string[] = [];
+  rightSelectedList: string[] = [];
+  
+  toggleList1Checkbox(item:any){
+    if(this.leftSelectedList.includes(item)){
+      this.leftSelectedList = this.leftSelectedList.filter((ele)=>ele !== item);
+    }else{
+      this.leftSelectedList.push(item);
+    }
+  }
+
+  toggleList2Checkbox(item:any){
+    if(this.rightSelectedList.includes(item)){
+      this.rightSelectedList = this.rightSelectedList.filter((ele)=>ele !== item);
+    }else{
+      this.rightSelectedList.push(item);
+    }
+  }
+
+  pushLeftToRight(){
+    this.list1 = this.list1.filter(item=>!this.leftSelectedList.includes(item));
+    this.list2 = [...this.list2,...this.leftSelectedList];
+    this.leftSelectedList = []
+    this.rightSelectedList = [];
+  }
+  pushRightToLeft(){
+    this.list2 = this.list2.filter(item=>!this.rightSelectedList.includes(item));
+    this.list1 = [...this.list1,...this.rightSelectedList];
+    this.leftSelectedList = []
+    this.rightSelectedList = [];
+  }
+
+
 }
